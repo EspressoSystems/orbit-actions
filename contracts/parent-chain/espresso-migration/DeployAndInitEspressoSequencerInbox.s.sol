09143b84b9da0df65eb6de7f20d4c0aa0360cdd7
Zach Showalter <zacshowa@gmail.com>
2024-11-20 21:33:51 -0500

    Remove old migration scripts and add replacements

--- contracts/parent-chain/espresso-migration/DeployAndInitEspressoSequencerInbox.s.sol
+++ contracts/parent-chain/espresso-migration/DeployAndInitEspressoSequencerInbox.s.sol
@@ -11,9 +11,10 @@ contract DeployAndInitEspressoSequencerInbox is Script {
         // Grab addresses from env
         address bridgeAddr = vm.envAddress("BRIDGE_ADDRESS");
         address reader4844Addr = vm.envAddress("READER_ADDRESS");
-      
+        address espressoTeeVerifierAddr = vm.envAddress("ESPRESSO_TEE_VERIFIER_ADDRESS");
+     
         // Grab the list of batch poster addresses valid for this rollup. This is a comma delimited list of addresses.
-        address[] batchPosters = vm.envAddress("BATCH_POSTER_ADDRS", ",");
+        address[] memory batchPosters = vm.envAddress("BATCH_POSTER_ADDRS", ",");
         address batchPosterManager = vm.envAddress("BATCH_POSTER_MANAGER");
 
         // Grab any uints we need to initialize the contract from envAddress
@@ -22,7 +23,7 @@ contract DeployAndInitEspressoSequencerInbox is Script {
         uint256 futureBlocks = vm.envUint("FUTURE_BLOCKS"); 
         uint256 delaySeconds = vm.envUint("DELAY_SECONDS");
         uint256 futureSeconds = vm.envUint("FUTURE_SECONDS");
-
+        uint256 maxDataSize = vm.envUint("MAX_DATA_SIZE");
         // Grab booleans we need from env
         bool isUsingFeeToken = vm.envBool("IS_USING_FEE_TOKEN");
 
@@ -31,20 +32,20 @@ contract DeployAndInitEspressoSequencerInbox is Script {
         IBridge bridge = IBridge(bridgeAddr);
         // Start broadcast to deploy and initializer the sequencerInbox
         vm.startBroadcast(deployerPrivateKey);
-        SequencerInbox sequencerInbox = new SequencerInbox(maxDataSize, IReader4844, isUsingFeeToken);
-        MaxTimeVariation maxTimeVariation = MaxTimeVariation({
-         delayblocks: delayBlocks,
+        SequencerInbox sequencerInbox = new SequencerInbox(maxDataSize, reader, isUsingFeeToken);
+        ISequencerInbox.MaxTimeVariation memory maxTimeVariation = ISequencerInbox.MaxTimeVariation({
+         delayBlocks: delayBlocks,
          futureBlocks: futureBlocks,
          delaySeconds: delaySeconds,
          futureSeconds: futureSeconds
         });
-        sequencerInbox.initialize(bridge, maxTimeVariation);
+        sequencerInbox.initialize(bridge, maxTimeVariation, espressoTeeVerifierAddr);
         // Setting batch posters and batch poster manager
-        for (uint256 i = 0; i < deployParams.batchPosters.length; i++) {
-            bridgeContracts.sequencerInbox.setIsBatchPoster(deployParams.batchPosters[i], true);
+        for (uint256 i = 0; i < batchPosters.length; i++) {
+            sequencerInbox.setIsBatchPoster(batchPosters[i], true);
         }
-        if (deployParams.batchPosterManager != address(0)) {
-            bridgeContracts.sequencerInbox.setBatchPosterManager(deployParams.batchPosterManager);
+        if (batchPosterManager != address(0)) {
+            sequencerInbox.setBatchPosterManager(batchPosterManager);
         }
         vm.stopBroadcast();
     }
