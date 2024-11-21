1783cd58612bcad571decf03a2f48481bd223df1
Zach Showalter <zacshowa@gmail.com>
2024-11-21 16:09:54 -0500

    add more comments to sequencer inbox migration and run yarn format

--- contracts/parent-chain/espresso-migration/EspressoSequencerInboxMigrationAction.sol
+++ contracts/parent-chain/espresso-migration/EspressoSequencerInboxMigrationAction.sol
@@ -13,6 +13,9 @@ import "nitro-contracts/rollup/IRollupLogic.sol";
 
 /// @notice Upgrades an Arbitrum orbit chain to use a SequencerInbox contract compatible with Espresso
 /// @dev _newSequencerInboxImpl: This is the address of the SequencerInbox implementation to point rollup's upgradeable proxy to.
+/// @dev _rollup: the address of the rollup to be migrated to the new SequencerInbox
+/// @dev _proxyAdminAddr: the address of the proxyAdmin for the rollup being migrated to the new SequencerInbox
+///      enable espresso confirmations at the end of the migration
 /// @dev    Modified from
 ///         https://github.com/ArbitrumFoundation/governance/blob/a5375eea133e1b88df2116ed510ab2e3c07293d3/src/gov-action-contracts/AIPs/ArbOS20/ArbOS20Action.sol
 contract EspressoSequencerInboxMigrationAction {
@@ -27,47 +30,48 @@ contract EspressoSequencerInboxMigrationAction {
     error SequencerInboxNotUpgraded(address oldSequencerInboxAddr);
 
     constructor(address _newSequencerInboxImpl, address _rollup, address _proxyAdminAddr) {
-      // If the new impl addresses are contracts, we need to revert  
-      if(!Address.isContract(_newSequencerInboxImpl)){
-          revert AddressIsNotContract(_newSequencerInboxImpl); 
-      }
+        // If the new impl addresses are contracts, we need to revert
+        if (!Address.isContract(_newSequencerInboxImpl)) {
+            revert AddressIsNotContract(_newSequencerInboxImpl);
+        }
 
-      if(!Address.isContract(_rollup)){
-          revert AddressIsNotContract(_rollup); 
-      }
+        if (!Address.isContract(_rollup)) {
+            revert AddressIsNotContract(_rollup);
+        }
 
-      if(!Address.isContract(_proxyAdminAddr)){
-          revert AddressIsNotContract(_proxyAdminAddr);
-      }
+        if (!Address.isContract(_proxyAdminAddr)) {
+            revert AddressIsNotContract(_proxyAdminAddr);
+        }
 
-      newSequencerInboxImpl = _newSequencerInboxImpl;
-      
-      rollup = _rollup;
+        newSequencerInboxImpl = _newSequencerInboxImpl;
 
-      proxyAdminAddr = _proxyAdminAddr;
+        rollup = _rollup;
 
+        proxyAdminAddr = _proxyAdminAddr;
     }
 
     function perform() public {
+        // set up contracts we need to interact with.
         IRollupCore rollupCore = IRollupCore(rollup);
         ProxyAdmin proxyAdmin = ProxyAdmin(proxyAdminAddr);
         TransparentUpgradeableProxy sequencerInbox =
             TransparentUpgradeableProxy(payable(address(rollupCore.sequencerInbox())));
-
+        // Get future blocks value to create assertion at end of migration
         (, uint256 futureBlocksBefore,,) = ISequencerInbox(address(sequencerInbox)).maxTimeVariation();
-        
+        // migrate the rollup to the new sequencer inbox
         proxyAdmin.upgrade(sequencerInbox, newSequencerInboxImpl);
 
         address proxyImpl = proxyAdmin.getProxyImplementation(sequencerInbox);
         // if the proxy implementation hasn't been updated, we need to revert.
         if (proxyImpl != newSequencerInboxImpl) {
-          revert SequencerInboxNotUpgraded(proxyImpl);
+            revert SequencerInboxNotUpgraded(proxyImpl);
         }
 
         (, uint256 futureBlocksAfter,,) = ISequencerInbox(address(sequencerInbox)).maxTimeVariation();
-        // if the max time variation was not set, the sequencer inbox is not initialized, we need to revert. 
-        if (futureBlocksBefore == 0 || futureBlocksBefore != futureBlocksAfter){
-          revert MaxTimeVariationNotSet();
+
+        // if the max time variation was not set, the sequencer inbox is not initialized, we need to revert.
+        if (futureBlocksBefore == 0 || futureBlocksBefore != futureBlocksAfter) {
+            revert MaxTimeVariationNotSet();
         }
     }
 }
