8246dbdf47d4a38f32aefc181372a9578d025802
Zach Showalter <zacshowa@gmail.com>
2025-01-13 15:25:02 -0500

    Remove dead code and update README.md

--- contracts/child-chain/espresso-migration/EspressoArbOSUpgrade.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.16;
-
-import "forge-std/Script.sol";
-import "../arbos-upgrade/UpgradeArbOSVersionAtTimestampAction.sol";
-
-contract EspressoArbOSUpgrade is UpgradeArbOSVersionAtTimestampAction, Script {
-    constructor(uint64 upgradeTimestamp) UpgradeArbOSVersionAtTimestampAction(35, upgradeTimestamp) {}
-}
