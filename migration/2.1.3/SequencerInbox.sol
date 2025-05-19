3e5fe33ebe75ccd402f8ad866706f47ef9138236
Sneh Koul <snehkoul1999@gmail.com>
2025-05-20 01:01:34 +0530

    update to use the latest sequencer inbox

--- migration/2.1.3/SequencerInbox.sol
+++ migration/2.1.3/SequencerInbox.sol
@@ -419,6 +419,29 @@ contract SequencerInbox is
     revert Deprecated();
   }
 
+  function _verifyAttestation(
+    uint256 sequenceNumber,
+    bytes calldata data,
+    uint256 afterDelayedMessagesRead,
+    IGasRefunder gasRefunder,
+    uint256 prevMessageCount,
+    uint256 newMessageCount,
+    bytes memory quote
+  ) private {
+    bytes32 reportDataHash = keccak256(
+      abi.encode(
+        sequenceNumber,
+        data,
+        afterDelayedMessagesRead,
+        address(gasRefunder),
+        prevMessageCount,
+        newMessageCount
+      )
+    );
+    espressoTEEVerifier.verify(quote, reportDataHash);
+    emit TEEAttestationQuoteVerified(sequenceNumber);
+  }
+
   function addSequencerL2BatchFromBlobs(
     uint256 sequenceNumber,
     uint256 afterDelayedMessagesRead,
@@ -434,6 +457,14 @@ contract SequencerInbox is
       uint256 blobGas
     ) = formBlobDataHash(afterDelayedMessagesRead);
 
+    // Reformat the stack to prevent "Stack too deep"
+    uint256 sequenceNumber_ = sequenceNumber;
+    bytes32 dataHash_ = dataHash;
+    uint256 afterDelayedMessagesRead_ = afterDelayedMessagesRead;
+    uint256 prevMessageCount_ = prevMessageCount;
+    uint256 newMessageCount_ = newMessageCount;
+    IBridge.TimeBounds memory timeBounds_ = timeBounds;
+
     // we use addSequencerL2BatchImpl for submitting the message
     // normally this would also submit a batch spending report but that is skipped if we pass
     // an empty call data size, then we submit a separate batch spending report later
@@ -443,27 +474,25 @@ contract SequencerInbox is
       bytes32 delayedAcc,
       bytes32 afterAcc
     ) = addSequencerL2BatchImpl(
-        dataHash,
-        afterDelayedMessagesRead,
+        dataHash_,
+        afterDelayedMessagesRead_,
         0,
-        prevMessageCount,
-        newMessageCount
+        prevMessageCount_,
+        newMessageCount_
       );
 
-    uint256 _sequenceNumber = sequenceNumber; // stack workaround
-
     // ~uint256(0) is type(uint256).max, but ever so slightly cheaper
-    if (seqMessageIndex != _sequenceNumber && _sequenceNumber != ~uint256(0)) {
-      revert BadSequencerNumber(seqMessageIndex, _sequenceNumber);
+    if (seqMessageIndex != sequenceNumber_ && sequenceNumber_ != ~uint256(0)) {
+      revert BadSequencerNumber(seqMessageIndex, sequenceNumber_);
     }
 
     emit SequencerBatchDelivered(
-      _sequenceNumber,
+      sequenceNumber_,
       beforeAcc,
       afterAcc,
       delayedAcc,
       totalDelayedMessagesRead,
-      timeBounds,
+      timeBounds_,
       IBridge.BatchDataLocation.Blob
     );
 
@@ -496,17 +525,41 @@ contract SequencerInbox is
     revert Deprecated();
   }
 
+  function _verifyBlobQuote(
+    uint256 sequenceNumber,
+    uint256 afterDelayedMessagesRead,
+    IGasRefunder gasRefunder,
+    uint256 prevMessageCount,
+    uint256 newMessageCount,
+    bytes memory quote
+  ) private {
+    bytes32[] memory dataHashes = reader4844.getDataHashes();
+    if (dataHashes.length == 0) revert MissingDataHashes();
+    bytes32 reportDataHash = keccak256(
+      abi.encode(
+        sequenceNumber,
+        afterDelayedMessagesRead,
+        address(gasRefunder),
+        prevMessageCount,
+        newMessageCount,
+        abi.encode(dataHashes)
+      )
+    );
+    espressoTEEVerifier.verify(quote, reportDataHash);
+    emit TEEAttestationQuoteVerified(sequenceNumber);
+  }
+
   /**
         Deprecated because we added a new method with TEE attestation quote
         to verify that the batch is posted by the batch poster running in TEE.
      */
   function addSequencerL2Batch(
-    uint256,
-    bytes calldata,
-    uint256,
+    uint256 sequenceNumber,
+    bytes calldata data,
+    uint256 afterDelayedMessagesRead,
     IGasRefunder gasRefunder,
-    uint256,
-    uint256
+    uint256 prevMessageCount,
+    uint256 newMessageCount
   ) external override refundsGas(gasRefunder, IReader4844(address(0))) {
     if (!isBatchPoster[msg.sender] && msg.sender != address(rollup))
       revert NotBatchPoster();
